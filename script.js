b66aeebfed5a92c78d022cae652135d6c88f0dd5
Pasha <vinsektor@gmail.com>
2023-08-15 22:41:37 +0300

    Complete layout

--- script.js
+++ script.js
@@ -1,51 +1,55 @@
-let player = () => {
-    let answer = prompt("Choose Rock, Paper or Scissors right now!");
-    return answer.toLowerCase();
-}
-
-let computer = () => {
-    let a = ["rock", "paper", "scissors"];
-    return a[Math.floor(Math.random() * a.length)];
-}
-
-let playRound = (playerGround, computerGround) => {
-    if (playerGround === computerGround) {
-        return [`It's a tie! You both chose ${playerGround}!`, "nor"]
-    }
-    else if (
-        (playerGround === "rock" && computerGround === "scissors") ||
-        (playerGround === "paper" && computerGround === "rock") ||
-        (playerGround === "scissors" && computerGround === "paper")
-        ) {
-        return [`You win! ${playerGround} beats ${computerGround}!`, 'player'];
-    } 
-    else {
-        return [`Computer wins! ${computerGround} beats ${playerGround}!`, "computer"];
-    }
+const player = () => {
+    const ANSWER = prompt('Choose Rock, Paper or Scissors right now!');
+    return ANSWER.toLowerCase();
 };
 
-let game = () => {
-    let p = 0,
-        c = 0;
+const computer = () => {
+    const A = ['rock', 'paper', 'scissors'];
+    return A[Math.floor(Math.random() * A.length)];
+};
+
+const playRound = (playerGround, computerGround) => {
+    if (playerGround === computerGround) {
+        return [`It's a tie! You both chose ${playerGround}!`, 'nor'];
+    } if (
+        (playerGround === 'rock' && computerGround === 'scissors')
+        || (playerGround === 'paper' && computerGround === 'rock')
+        || (playerGround === 'scissors' && computerGround === 'paper')
+    ) {
+        return [`You win! ${playerGround} beats ${computerGround}!`, 'player'];
+    }
+    return [`Computer wins! ${computerGround} beats ${playerGround}!`, 'computer'];
+};
+
+const game = () => {
+    let p = 0;
+    let c = 0;
     for (let i = 1; i <= 5; i++) {
-        let response = playRound(player(), computer());
+        const response = playRound(player(), computer());
         console.log(response[0]);
-        if (response[1] === "player") {
-            p++;
-        }
-        else if (response[1] === "computer") {
-            c++;
+        if (response[1] === 'player') {
+            p += 1;
+        } else if (response[1] === 'computer') {
+            c += 1;
         }
     }
     if (p > c) {
-        return "You are winner!"
+        return 'You are winner!';
+    } if (p < c) {
+        return 'Computer is winner!';
     }
-    else if (p < c) {
-        return "Computer is winner!"
-    }
-    else {
-        return "Tie!"
-    }
+    return 'Tie!';
 };
 
-console.log(game());
+// const opt = document.querySelectorAll('.options span');
+// function changeOpacity(e) {
+//     this.classList.add('.option_target');
+// }
+
+// function removeOpacity(e) {
+//     this.classList.remove('.option_target');
+// }
+
+// opt.forEach((element) => {
+//     element.addEventListener('mouseover', changeOpacity);
+// });
