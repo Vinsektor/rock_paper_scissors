72c793ca3558bbbdaa632791bb7d574ad53bfe9a
Pasha <vinsektor@gmail.com>
2023-08-16 22:12:28 +0300

    Made a base functional for game

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const player = () => {
-    const ANSWER = prompt('Choose Rock, Paper or Scissors right now!');
-    return ANSWER.toLowerCase();
-};
+const gameContainer = document.querySelector('.container');
+const userResult = document.querySelector('.player_hand img');
+const cpuResult = document.querySelector('.comp_hand img');
+const RESULT = document.querySelector('.result');
+const optionImages = document.querySelectorAll('.option_image');
+
+// const player = () => {
+//     const ANSWER = prompt('Choose Rock, Paper or Scissors right now!');
+//     return ANSWER.toLowerCase();
+// };
 
 const computer = () => {
     const A = ['rock', 'paper', 'scissors'];
@@ -26,6 +32,7 @@ const game = () => {
     let c = 0;
     for (let i = 1; i <= 5; i++) {
         const response = playRound(player(), computer());
+        // eslint-disable-next-line no-console
         console.log(response[0]);
         if (response[1] === 'player') {
             p += 1;
@@ -41,15 +48,42 @@ const game = () => {
     return 'Tie!';
 };
 
-// const opt = document.querySelectorAll('.options span');
-// function changeOpacity(e) {
-//     this.classList.add('.option_target');
-// }
+optionImages.forEach((image, index) => {
+    image.addEventListener('click', (e) => {
+        let ans = '';
+        image.classList.add('active');
+        setTimeout(() => image.classList.remove('active'), 1500);
+        // Define choosing
+        if (index === 0) {
+            ans = 'rock';
+        } else if (index === 1) {
+            ans = 'paper';
+        } else {
+            ans = 'scissors';
+        }
 
-// function removeOpacity(e) {
-//     this.classList.remove('.option_target');
-// }
+        // eslint-disable-next-line no-multi-assign
+        userResult.src = 'images/rock.png';
+        cpuResult.src = 'images/rock.png';
+        RESULT.textContent = 'Wait...';
 
-// opt.forEach((element) => {
-//     element.addEventListener('mouseover', changeOpacity);
-// });
+        gameContainer.classList.add('start');
+
+        setTimeout(() => {
+            const cpuImages = {
+                rock: 'images/rock.png',
+                paper: 'images/paper.png',
+                scissors: 'images/scissors.png',
+            };
+            const compAns = computer();
+            const response = playRound(ans, compAns);
+            const imageSrc = e.target.src;
+
+            gameContainer.classList.remove('start');
+            RESULT.textContent = response[0];
+
+            userResult.src = imageSrc;
+            cpuResult.src = cpuImages[`${compAns}`];
+        }, 1900);
+    });
+});
